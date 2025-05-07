5d1a80d50b1fcbdb66eb39e1263427832ba993f9
eoeosb <eoeosb@gmail.com>
2025-05-07 10:20:38 +0900

    change to darkmode app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/landing_screen.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  if (kIsWeb) {
+    SharedPreferences.setPrefix('llm_front_');
+  }
+  
+  await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
 
@@ -13,26 +22,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '뉴스 검색',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        cardTheme: CardTheme(
+          color: Colors.grey[900],
+          elevation: 2,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[900],
+          elevation: 0,
+        ),
+        scaffoldBackgroundColor: Colors.grey[850],
+        navigationBarTheme: NavigationBarThemeData(
+          backgroundColor: Colors.grey[900],
+          indicatorColor: Colors.blue.withAlpha(51),
+        ),
       ),
-      home: const HomeScreen(),
+      home: const LandingScreen(),
     );
   }
 }
